3baf5ed11963ec61f9baaa5d1a19e4e3b794ced3
theapache64 <theapache64@gmail.com>
2024-09-03 19:02:41 +0530

    💚 first commit - with build error

---
+++ app/src/main/java/com/github/theapache64/lintrepro/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.github.theapache64.lintrepro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // this will fail the build
+    fun myFunc(param : String){
+        println("QuickTag: MainActivity:myFunc: ")
+    }
+}
+
+sealed interface Foo
+
+// but this won't
+object Bar : Foo
